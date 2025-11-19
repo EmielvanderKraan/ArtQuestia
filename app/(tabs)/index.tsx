1cec8255b3caca03d9ed8970f59832474700aae1
Emiel van der Kraan <emiel.van.der.kraan@student.howest.be>
2025-11-19 15:17:49 +0100

    Home tab: searchbar, buttons and text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { ActivityIndicator, StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
+import React from 'react';
+import { ActivityIndicator, Image, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
+// 👉 Pas deze aan naar jouw PNG's
+import Icon1 from '../../assets/icons/route.png';
+import Icon3 from '../../assets/icons/share.png';
+import Icon2 from '../../assets/icons/stickers.png';
+
 export default function SettingsScreen() {
-  // Load custom fonts
   const [fontsLoaded] = useFonts({
     Impact: require('../../assets/fonts/impact.ttf'),
     LeagueSpartan: require('../../assets/fonts/LeagueSpartan-VariableFont_wght.ttf'),
   });
 
-  // Show a loading indicator until fonts are ready
   if (!fontsLoaded) {
     return <ActivityIndicator size="large" style={styles.loader} />;
   }
@@ -25,11 +28,66 @@ export default function SettingsScreen() {
       >
         ArtQuestia
       </ThemedText>
+
       <ThemedText
         type="title"
         style={[styles.subtitle, { fontFamily: 'LeagueSpartan' }]}
       >
         Beleef, ontdek, verbind
+      </ThemedText>
+      {/* Zoekbalk */}
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Zoek naar kunstwerken"
+          placeholderTextColor="#999"
+          style={[styles.input, { fontFamily: 'LeagueSpartan' }]}
+        />
+
+        <TouchableOpacity style={styles.searchButton}>
+          <Image
+            source={require('../../assets/icons/search.png')}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      </View>
+      <ThemedText
+        type="title"
+        style={[styles.title, { fontFamily: 'LeagueSpartan' }]}
+      >
+        Begin de zoektocht!
+      </ThemedText>
+
+      <View style={styles.rowButtons}>
+
+        {/* button 1 */}
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => { }}>
+          <Image source={Icon1} style={styles.buttonIcon} />
+          <View style={styles.button}>
+            <ThemedText style={styles.buttonText}>Kies je route</ThemedText>
+          </View>
+        </TouchableOpacity>
+
+        {/* button 2 */}
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => { }}>
+          <Image source={Icon2} style={styles.buttonIcon} />
+          <View style={styles.button}>
+            <ThemedText style={styles.buttonText}>Verzamel stickers!</ThemedText>
+          </View>
+        </TouchableOpacity>
+
+        {/* button 3 */}
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => { }}>
+          <Image source={Icon3} style={styles.buttonIcon} />
+          <View style={styles.button}>
+            <ThemedText style={styles.buttonText}>Deel je ervaring</ThemedText>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <ThemedText
+        type="title"
+        style={[styles.title, { fontFamily: 'LeagueSpartan' }]}
+      >
+        Dichtstbijzijnde kunstwerken
       </ThemedText>
     </ThemedView>
   );
@@ -51,12 +109,84 @@ const styles = StyleSheet.create({
   },
   mainTitle: {
     fontSize: 32,
-    // Optional: add color or other styles
     color: '#fff',
   },
   subtitle: {
     fontSize: 16,
     marginTop: 8,
     color: '#fff',
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 8,
+    color: '#fff',
+  },
+
+  rowButtons: {
+    flexDirection: 'row',
+    marginTop: 40,
+    marginBottom: 20,
+    gap: 9,
+  },
+
+  buttonContainer: {
+    alignItems: 'center',
+    position: 'relative',
+    width: 115,
+  },
+
+  // Image boven de button
+  buttonIcon: {
+    width: 50,
+    height: 50,
+    position: 'absolute',
+    top: -25,
+    zIndex: 10,
+  },
+
+  // De button
+  button: {
+    width: '100%',
+    paddingVertical: 10,
+    borderRadius: 14,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8,
+    backgroundColor: '#292929',
+    paddingTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 15,
+    fontFamily: 'LeagueSpartan',
+  },
+  container: {
+    flexDirection: 'row',
+    width: '95%',
+    height: 45,
+    backgroundColor: '#fff',
+    borderRadius: 30,
+    overflow: 'hidden', // zodat het oranje deel ook mooi afgerond blijft
+  },
+
+  input: {
+    flex: 1,
+    paddingLeft: 15,
+    fontSize: 15,
+    color: '#000',
+  },
+
+  searchButton: {
+    width: 50,
+    backgroundColor: '#FF7A00', // oranje
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  icon: {
+    width: 18,
+    height: 18,
+    tintColor: '#fff',
   },
 });
